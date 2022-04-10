2f174481d16227fcda42d18a20c1580b96b34a16
Shruti Goel <85050516+Shruti-2211@users.noreply.github.com>
2022-04-10 15:33:50 +0530

    Add files via upload

---
+++ Hierarchial.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 13 21:06:33 2021
+
+@author: SNB
+"""
+
+# Hierarchical Clustering
+
+# Importing the libraries
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+
+# Importing the dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+# Using the dendrogram to find the optimal number of clusters
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
+
+# Fitting Hierarchical Clustering to the dataset
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = 'ward')
+y_hc = hc.fit_predict(X)
+
+#Checking Accuracy
+from sklearn import metrics
+from sklearn.metrics.cluster import adjusted_rand_score
+
+print("\n-----------------------HIERARCHIAL CLUSTERING----------------------- \n")
+print("Accuracy:", metrics.accuracy_score(y, y_hc))
+print("Quality of clusters in Hierarchial Clustering")
+print("Adjusted Rand Score:", adjusted_rand_score(y, y_hc))
+
+from sklearn.metrics import silhouette_score
+labels = hc.labels_
+print("Silhouette Score", silhouette_score(X, labels, metric = 'euclidean'))
+
+# Visualising the clusters
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
+plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
