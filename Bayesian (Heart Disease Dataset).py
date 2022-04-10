4cd9110437e3fa47000d8b7dd4f1253c49434d7b
Shruti Goel <85050516+Shruti-2211@users.noreply.github.com>
2022-04-10 15:40:29 +0530

    Update Bayesian (Heart Disease Dataset).py

--- Bayesian (Heart Disease Dataset).py
+++ Bayesian (Heart Disease Dataset).py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec 13 00:01:12 2021
-
-@author: SNB
-"""
-
 import numpy as np
 import pandas as pd
 from pgmpy.estimators import MaximumLikelihoodEstimator
